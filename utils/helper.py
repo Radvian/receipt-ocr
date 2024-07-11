0c325fe7883f54b4c9f1891be0c9a39e164e2d53
Grady Matthias Oktavian <gradyoktavian@gmail.com>
2024-07-11 13:58:34 +0700

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -9,33 +9,21 @@ from google.cloud import vision
 from .prompt import input_prompt_item_price_info, input_prompt_payment_info, input_prompt_tenant_info
 import streamlit as st
 # import asyncio
+from google.oauth2 import service_account
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["connection_gcloud"]
+)
+vision_client = vision.ImageAnnotatorClient(credentials=credentials)
+
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-
-load_dotenv()
-google_credentials = {
-  "type": "service_account",
-  "project_id": "boxwood-diagram-426306-s3",
-  "private_key_id": "08d2ae3cca77c6c2e3d0ca4aaa0f48a59bd122c2",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQDUeMzPURzAh3jn\nUMVOlO/2JiWnlluYR0AUiMDBA6xHCSxXNt6rgHN7ADI++5zhkjdyEUgn0IUzEyXX\nGzNHNvWY0mPDqp75giQ6k0FeIsVpidnMWW9i1I6vIH/dhwjnZ2Eb+10SH/Diun58\nxc4DROS+mwbxWrhu0z2fGYJniCGf/ktflk9b9gn6ICHsq9WNF3V7tsTKageYKoVO\nlPkKustbd+HZ9QRhPdBzs8H6SZxUHt2q6XCJ6GOTN3JsaFWw79RbNHGCRfL1d424\nJBknYXmAFujO6RMZYAxP46MaH9qcM+2Q4eFOwvFMRttoh8+e9N7AJxVBCE5VtUCi\nhlGrfM7vAgMBAAECggEABAQJ0EahhdIJqvWP2nMI21zP4WGqIYVXOgwg+ITv600f\nwBY9ZyhqW6zJRlTNxkWpM/K3q75Ohi9FISerycYe0OCeb22jdjvLdiqfqv56lLVn\nnxZ2+yXhUdYKFKR0uqBC6ZslSy5nXcxV5QMoEvppCGE4ZF9pP6RRHyywQPIFu1cI\n6rt7vucRqjN7/CYL7/OBsMJq21oWAWhG91scn7HR7C4reXKwJEPL3L+JrGEkPBeg\npdykAxUax2ek0CIak/0LYiU8hmwL0PRHvEOyFTpJFPoTXGzqZ3Ed2JKW38l/sTYb\n/Z4gAidDrLvBMbJdILUHqcocM1Ltcm2K4MDQZOdyoQKBgQDzSeaIHknoVpznFCs/\nZjExP9sefxwDIH9NLl/kba90lVDFAwxFSujZnDpx3S8tTI7Ui+7iS6zp5rLdKMnO\nMjGc5vegdi9zm8SDJiWlwCZQIIf+C+/FEmJAhcWz4aUXbWaQzp7H73TDLePwlpla\nvCfbEvvzZdDxX72p4QHoiwC+DQKBgQDfkrXhM7iwq4rE+s26HRgUgY2+dCtKkNMZ\nVp2GHg4JM88Az5K3lnuEXp/xVh1WhlnfM+5RrQ3l3Z5w74UrBh+e9Vl5fPOW15za\ngmeB0T9ebyn9iMW9jqsiy2ef6vf6AuP8XhH87nlXdDR/4ForgId+0o3spsN3aRYu\nq0KWyop96wKBgHCR3S1WdnQev3LcjjZDnrdfCJBP2DJKMx0PPCeB6RFUlY7THjZh\ntuQZuaE96rpXFi+an+ntghCoQlK6sMXLy9EBvcptZP9lL8RPPlQ3WILyynnuI/9X\nxkZ8n0HgQndYk4ClWirAzZISBcoopMSrEvX3DsVMcyV8W1HjXvKufqVxAn8+4QRG\nUkWfap+cxu0VaxlaqxUye3mxfwngFnw3PDy2WdveXoLlH22dzP8u4QlfTGn6XKCq\nWAGQpMPq/3J0p0Q9cMO5Z2sbAYkb65Ew8ajg5mLo646xwW5gWtTKJUuZBTcZw1tL\nx8ZY0lXxoSe7FGsHQDCqXjybWPLkLUaNImnRAoGBAPC5jOkxyxgxmE6/7CTB4h0F\n3ozpmYqo2N5AkoKDSDSd2LPWaT/dNi8ns78rSVh6X2E+8tq71gCPjThn6ARpE6QH\nUq3setwncQdFOinMPty3PqI2tAC0aXe5VmHq4kH9hQLgjfSbAP8kBQo5TKnP4V/g\nuhTSywm3bMpOJRb7oyXF\n-----END PRIVATE KEY-----\n",
-  "client_email": "ocrservice@boxwood-diagram-426306-s3.iam.gserviceaccount.com",
-  "client_id": "108422349427166697157",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/ocrservice%40boxwood-diagram-426306-s3.iam.gserviceaccount.com",
-  "universe_domain": "googleapis.com"
-}
-
-openai_api_key = OPENAI_API_KEY
-# Set API keys for OpenAI and Google Vision
-openai.api_key = openai_api_key
-os.environ['OPENAI_API_KEY'] = openai_api_key
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = google_credentials
+openai.api_key = OPENAI_API_KEY
+os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
 
 # Initialize OpenAI and Google Vision clients
 openai_client = OpenAI()
 instructor_client = instructor.from_openai(openai_client)
-vision_client = vision.ImageAnnotatorClient()
 
 def numberstring_convert(numstring):
     if type(numstring) == str:
